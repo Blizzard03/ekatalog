b42ea1277c18493af38df2e546b4bb98ed4d2a33
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2023-11-09 12:09:33 +0700

    reconfigure the project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:ekatalog/providers/auth_provider.dart';
 import 'package:ekatalog/screens/login_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
